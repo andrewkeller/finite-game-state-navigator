f5eef1ad5a26ed3fcf89d3e81928e072f9c64129
Andrew Keller <andrew.keller@covenanteyes.com>
2012-09-16 23:55:04 -0400

    Added a blank Xcode project.

---
+++ src/tic-tac-toe-state-navigator/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  tic-tac-toe-state-navigator
+//
+//  Created by Andrew Keller on 9/16/12.
+//  Copyright (c) 2012 Andrew Keller. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
